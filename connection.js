12b39df63f5037e4301f535de54caba40b12f704
zviangi10 <nino.gorgadze93@gmail.com>
2024-08-26 16:28:21 -0400

    connection

---
+++ connection.js
@@ -0,0 +1,16 @@
+// config/connection.js
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT,
+  }
+);
+
+module.exports = sequelize;
