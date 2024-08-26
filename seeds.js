c7456ad2c0ba391b74ae7cdbf31e7f186ca078fc
zviangi10 <nino.gorgadze93@gmail.com>
2024-08-26 16:30:57 -0400

    seeds

---
+++ seeds.js
@@ -0,0 +1,16 @@
+// seeds/seed.js
+const sequelize = require('../config/connection');
+const seedCategories = require('./category-seeds');
+const seedProducts = require('./product-seeds');
+const seedTags = require('./tag-seeds');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  await seedCategories();
+  await seedProducts();
+  await seedTags();
+  console.log('All seeds planted!');
+  process.exit(0);
+};
+
+seedAll();
